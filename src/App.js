390ad119a0220ea3ee469b99084766bdeaf0d867
kanokenji <aleksandrtarasevich77@gmail.com>
2020-06-10 13:45:26 -0600

    Add project to gh pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import {Table} from "react-bootstrap";
 import EditData from "./EditData/EditData";
 
-const apiUrl = "https://jsonplaceholder.typicode.com/users/";
+// const apiUrl = "https://jsonplaceholder.typicode.com/users/";
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
 function App() {
 
